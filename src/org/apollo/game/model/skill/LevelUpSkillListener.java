cb82158039743b8c9077cfa79418db694769f0a9
atomicint <ryley.kimmel@live.com>
2014-02-21 01:43:39 -0500

    Finished LevelUpSkillListener

--- src/org/apollo/game/model/skill/LevelUpSkillListener.java
+++ src/org/apollo/game/model/skill/LevelUpSkillListener.java
@@ -1,14 +1,15 @@
 
 package org.apollo.game.model.skill;
 
-import org.apollo.game.event.impl.OpenInterfaceEvent;
+import org.apollo.game.event.impl.OpenChatboxOverlayEvent;
+import org.apollo.game.event.impl.SetInterfaceTextEvent;
 import org.apollo.game.model.Player;
 import org.apollo.game.model.Skill;
 import org.apollo.game.model.SkillSet;
 import org.apollo.util.LanguageUtil;
 
 /**
- * A {@link SkillListener} which notifies the player when they have levelled up
+ * A {@link SkillListener} which notifies the player when they have leveled up
  * a skill.
  * @author Graham
  */
@@ -18,15 +19,10 @@ public final class LevelUpSkillListener extends SkillAdapter
 	/**
 	 * The interface id's for a normal level up in a skill
 	 */
+	/* XXX: It's too bad these ids aren't in order by skill id or else this would be easier. */
 	private static final int[] LEVEL_UP_IDS = {
-
-	};
-
-	/**
-	 * The interface id's for when you get the maximum level in a skill
-	 */
-	private static final int[] MAXIMUM_LEVEL_IDS = {
-
+		157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170,
+		171, 172, 173, 174, 175, 176, 177
 	};
 
 	/**
@@ -51,8 +47,13 @@ public final class LevelUpSkillListener extends SkillAdapter
 		String name = Skill.getName( id );
 		String article = LanguageUtil.getIndefiniteArticle( name );
 		int level = skill.getMaximumLevel();
-		player.send( new OpenInterfaceEvent( level == 99 ? MAXIMUM_LEVEL_IDS[ id ]: LEVEL_UP_IDS[ id ] ) );
+		player.send( new OpenChatboxOverlayEvent( LEVEL_UP_IDS[ id ] ) );
+		player.send( new SetInterfaceTextEvent( 4268, "Congratulations! You've just advanced " + article + " " + name + " level!" ) );
+		player.send( new SetInterfaceTextEvent( 4269, "You have now reached level " + level + "!" ) );
 		player.sendMessage( "You've just advanced " + article + " " + name + " level! You have reached level " + level + "." );
+		if( level == 99 ) {
+			player.sendMessage( "@dre@Well done! You've achieved the highest possible level in this skill." );
+		}
 	}
 
 }
