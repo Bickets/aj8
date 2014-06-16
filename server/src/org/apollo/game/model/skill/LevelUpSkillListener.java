ba4d41fe1d91734af83b56a21624f90697d76e61
atomicint <ryley.kimmel@live.com>
2014-06-16 04:56:06 -0500

    Removed hard-coded level up interface

--- server/src/org/apollo/game/model/skill/LevelUpSkillListener.java
+++ server/src/org/apollo/game/model/skill/LevelUpSkillListener.java
@@ -1,16 +1,10 @@
 package org.apollo.game.model.skill;
 
-import java.util.List;
-
 import org.apollo.game.model.Graphic;
 import org.apollo.game.model.Player;
 import org.apollo.game.model.Skill;
 import org.apollo.game.model.SkillSet;
-import org.apollo.game.msg.impl.OpenDialogueInterfaceMessage;
-import org.apollo.game.msg.impl.SetInterfaceTextMessage;
 import org.apollo.util.LanguageUtil;
-
-import static java.util.Arrays.asList;
 
 /**
  * A {@link SkillListener} which notifies the player when they have leveled up a
@@ -19,13 +13,6 @@ import static java.util.Arrays.asList;
  * @author Graham
  */
 public final class LevelUpSkillListener extends SkillAdapter {
-
-    /**
-     * The interface id's for a normal level up in a skill
-     */
-    private static final int[] LEVEL_UP_IDS = { 6247, 6253, 6206, 6216, 4443,
-	    6242, 6211, 6226, 4272, 6231, 6258, 4282, 6263, 6221, 4416, 6237,
-	    4277, 4261, 12122, 4887, 4267, };
 
     /**
      * The player.
@@ -41,30 +28,11 @@ public final class LevelUpSkillListener extends SkillAdapter {
 	this.player = player;
     }
 
-    private List getChildren(int id) {
-	switch (id) {
-	case 4443:
-	    return asList(5453, 6114);
-	case 4416:
-	    return asList(4417, 4438);
-	case 4261:
-	    return asList(4263, 4264);
-	case 4887:
-	    return asList(4890, 4891);
-	}
-	return asList(id + 1, id + 2);
-    }
-
     @Override
     public void leveledUp(SkillSet set, int id, Skill skill) {
 	String name = Skill.getName(id);
 	String article = LanguageUtil.getIndefiniteArticle(name);
 	int level = skill.getMaximumLevel();
-	int interfaceId = LEVEL_UP_IDS[id];
-	List<Integer> children = getChildren(interfaceId);
-	player.send(new SetInterfaceTextMessage(children.get(0), "Congratulations! You've just advanced " + article + " " + name + " level!"));
-	player.send(new SetInterfaceTextMessage(children.get(1), "You have now reached level " + level + "!"));
-	player.send(new OpenDialogueInterfaceMessage(interfaceId));
 	player.sendMessage("You've just advanced " + article + " " + name + " level! You have reached level " + level + ".");
 	if (level == 99) {
 	    player.sendMessage("Well done! You've achieved the highest possible level in this skill.");
