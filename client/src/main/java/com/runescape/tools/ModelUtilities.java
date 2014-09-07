4a840227bc41d9de703330f18ebcf80c08127e01
atomicint <ryley.kimmel@live.com>
2014-09-07 14:24:31 -0400

    Use File instead of Path, unnecessary conversion to File from Path

--- client/src/main/java/com/runescape/tools/ModelUtilities.java
+++ client/src/main/java/com/runescape/tools/ModelUtilities.java
@@ -4,8 +4,6 @@ import static java.lang.Integer.parseInt;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import com.runescape.media.renderable.Model;
 import com.runescape.util.Signlink;
@@ -20,7 +18,7 @@ public final class ModelUtilities {
 	/**
 	 * The path to where external models are located.
 	 */
-	private static final Path PATH = Paths.get(Signlink.getRootDirectory() + "models");
+	private static final File ROOT = new File(Signlink.getRootDirectory() + "models");
 
 	/**
 	 * Loads all of the models within the specified {@link #PATH}.
@@ -28,9 +26,8 @@ public final class ModelUtilities {
 	 * @throws IOException If some I/O exception occurs.
 	 */
 	public static void loadModels() throws IOException {
-		File file = PATH.toFile();
-		for (String name : file.list()) {
-			Model.loadModelHeader(FileUtilities.getBytesFromFile(file), parseInt(name));
+		for (File file : ROOT.listFiles()) {
+			Model.loadModelHeader(FileUtilities.getBytesFromFile(file), parseInt(file.getName()));
 		}
 	}
 
