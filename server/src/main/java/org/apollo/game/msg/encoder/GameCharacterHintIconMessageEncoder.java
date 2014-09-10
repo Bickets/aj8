2557a5f2a9971cc0258cf0d1cc1de5e3e42aa9e6
atomicint <ryley.kimmel@live.com>
2014-09-10 00:24:09 -0400

    Fixed James' hint icon messages

--- server/src/main/java/org/apollo/game/msg/encoder/GameCharacterHintIconMessageEncoder.java
+++ server/src/main/java/org/apollo/game/msg/encoder/GameCharacterHintIconMessageEncoder.java
@@ -3,6 +3,7 @@ package org.apollo.game.msg.encoder;
 import org.apollo.game.msg.MessageEncoder;
 import org.apollo.game.msg.annotate.EncodesMessage;
 import org.apollo.game.msg.impl.GameCharacterHintIconMessage;
+import org.apollo.net.codec.game.DataOrder;
 import org.apollo.net.codec.game.DataType;
 import org.apollo.net.codec.game.GamePacket;
 import org.apollo.net.codec.game.GamePacketBuilder;
@@ -19,7 +20,7 @@ public final class GameCharacterHintIconMessageEncoder implements MessageEncoder
 	public GamePacket encode(GameCharacterHintIconMessage message) {
 		GamePacketBuilder builder = new GamePacketBuilder(254);
 		builder.put(DataType.BYTE, message.getType().getIndex());
-		builder.put(DataType.BYTE, message.getCharacter().getIndex());
+		builder.put(DataType.SHORT, DataOrder.LITTLE, message.getCharacter().getIndex());
 		return builder.toGamePacket();
 	}
 
