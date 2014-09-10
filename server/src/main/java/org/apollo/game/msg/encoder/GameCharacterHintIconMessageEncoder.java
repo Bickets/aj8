9df8bbe4f3764a7dd22e03ac0fffe77bdbf8ec3a
atomicint <ryley.kimmel@live.com>
2014-09-10 02:23:03 -0400

    For whatever reason the client is trying to read 6 bytes rather than 3, this caused a crash. Sending 'junk' seems to be the only work-around for this currently.

--- server/src/main/java/org/apollo/game/msg/encoder/GameCharacterHintIconMessageEncoder.java
+++ server/src/main/java/org/apollo/game/msg/encoder/GameCharacterHintIconMessageEncoder.java
@@ -3,7 +3,6 @@ package org.apollo.game.msg.encoder;
 import org.apollo.game.msg.MessageEncoder;
 import org.apollo.game.msg.annotate.EncodesMessage;
 import org.apollo.game.msg.impl.GameCharacterHintIconMessage;
-import org.apollo.net.codec.game.DataOrder;
 import org.apollo.net.codec.game.DataType;
 import org.apollo.net.codec.game.GamePacket;
 import org.apollo.net.codec.game.GamePacketBuilder;
@@ -20,7 +19,8 @@ public final class GameCharacterHintIconMessageEncoder implements MessageEncoder
 	public GamePacket encode(GameCharacterHintIconMessage message) {
 		GamePacketBuilder builder = new GamePacketBuilder(254);
 		builder.put(DataType.BYTE, message.getType().getIndex());
-		builder.put(DataType.SHORT, DataOrder.LITTLE, message.getCharacter().getIndex());
+		builder.put(DataType.SHORT, message.getCharacter().getIndex());
+		builder.put(DataType.TRI_BYTE, 0);
 		return builder.toGamePacket();
 	}
 
